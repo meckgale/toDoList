1e8ad957e053ae87b0687ec4b70fa48480778135
meck <meckgalen@gmail.com>
2023-09-07 00:17:40 +0300

    project is ready to start

---
+++ dist/bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunktodolist"] = self["webpackChunktodolist"] || []).push([["bundle"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("It's alive")
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkl0J3MgYWxpdmVcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
